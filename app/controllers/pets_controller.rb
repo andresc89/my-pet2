93888c705f880aba47c8ac28aba61235ca28c31c
David Deus <davidafonsodeus@outlook.com>
2022-11-22 10:30:11 +0000

    pundit setup start

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,12 @@
 class PetsController < ApplicationController
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name)
+  end
 end
