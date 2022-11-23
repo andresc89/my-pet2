c6af846fc126f29ccc1493a561af91e486f16213
David Deus <davidafonsodeus@outlook.com>
2022-11-23 15:31:37 +0000

    comminting first

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-<<<<<<< HEAD
-  resources :pets do
-    resources :bookings # , only: [:new, :create, :index, :show, :update, :edit, :destroy]
-=======
   resources :pets, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+
     collection do
       get :my_pets
     end
->>>>>>> 59b35f7f7268b9eda6539f4aa65fc2e6ef276f85
   end
 end
